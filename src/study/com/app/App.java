9a2ff0e70091fd540700dfc46340397066bc5368
123748712 <kemco01@naver.com>
2021-11-14 17:11:53 +0900

    Article List 검색기능 추가

--- src/study/com/app/App.java
+++ src/study/com/app/App.java
@@ -42,11 +42,35 @@ public class App {
 				System.out.println("제목 : " + title);
 				System.out.println("내용 : " + body);
 
-			} else if (command.equals("article list")) {
+			} else if (command.startsWith("article list")) {
 				System.out.println("게시글 리스트 기능을 구현합니다.");
 
+				if (articles.size() == 0) {
+					System.out.println("게시글이 존재하지 않습니다.");
+					continue;
+				}
+
+				String searchKeyword = command.substring("article list".length()).trim();
+
+				List<Article> searchedArticles = new ArrayList<>();
+
+				if (searchKeyword.length() > 0) {
+					for (Article article : articles) {
+						if (article.title.contains(searchKeyword)) {
+							searchedArticles.add(article);
+						}
+					}
+				}
+
+				if (searchedArticles.size() == 0) {
+					System.out.println("검색된 게시글이 존재하지 않습니다.");
+					continue;
+				} else {
+					searchedArticles = articles;
+				}
+
 				System.out.println("제목  |  내용  |  조회수");
-				for (Article article : articles) {
+				for (Article article : searchedArticles) {
 					System.out.println(article.id + " | " + article.title + " | " + article.hit);
 				}
 			} else if (command.startsWith("article detail ")) {
@@ -60,6 +84,7 @@ public class App {
 
 				if (foundId == 0) {
 					System.out.println("숫자만 입력해주세요.");
+					continue;
 				}
 
 				Article foundArticle = getFoundArticleById(foundId);
@@ -114,9 +139,9 @@ public class App {
 					System.out.println("숫자만 입력해주세요.");
 				}
 
-				Article foundArticle = getFoundArticleById(foundId);;
+				Article foundArticle = getFoundArticleById(foundId);
+				;
 
-				
 				if (foundArticle == null) {
 					System.out.println("게시글이 존재하지 않습니다.");
 					continue;
