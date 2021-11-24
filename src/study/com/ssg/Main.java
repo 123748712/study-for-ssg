c9e7444366ec778e82552509b3aad56ef79cdbc8
123748712 <kemco01@naver.com>
2021-11-24 15:33:07 +0900

    초기화

--- src/study/com/ssg/Main.java
+++ src/study/com/ssg/Main.java
@@ -1,15 +1,8 @@
 package study.com.ssg;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import study.com.app.App;
-import study.com.util.Util;
-
 public class Main {
 
 	public static void main(String[] args) {
-		new App().start();
+
 	}
 }
