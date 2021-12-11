cf550076e8cd277462d0e2050da5294179215fff
123748712 <kemco01@naver.com>
2021-12-11 20:14:54 +0900

    article write 기능 구현

--- src/study/com/ssg/Main.java
+++ src/study/com/ssg/Main.java
@@ -1,7 +1,51 @@
 package study.com.ssg;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+		List<Article> articles = new ArrayList<>();
+		System.out.println("=== 프로그램 시작 ===");
+
+		while (true) {
+			System.out.print("명령어를 입력해주세요 : ");
+			String command = scanner.nextLine();
+
+			if (command.equals("article write")) {
+				System.out.println("게시글 작성 기능을 구현합니다.");
+
+				System.out.print("제목 : ");
+				String title = scanner.nextLine();
+				System.out.print("내용 : ");
+				String body = scanner.nextLine();
+
+				Article article = new Article(title, body);
+
+				articles.add(article);
+
+				System.out.println(article.articleId + "번 게시글 작성이 완료되었습니다.");
+			} else {
+				System.out.println("잘못된 명령어입니다");
+				continue;
+			}
+		}
+	}
+}
+
+class Article {
+	static int index;
+	int articleId;
+	String title;
+	String body;
+
+	Article(String title, String body) {
+		this.index++;
+		this.articleId = index;
+		this.title = title;
+		this.body = body;
 	}
 }
