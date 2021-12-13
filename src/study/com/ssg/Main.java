f0fc13f02725ef0140099a704a68a87000333704
123748712 <kemco01@naver.com>
2021-12-13 18:11:27 +0900

    article detail 기능 구현:

--- src/study/com/ssg/Main.java
+++ src/study/com/ssg/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import study.com.util.Util;
+
 public class Main {
 	public static void main(String[] args) {
 
@@ -31,7 +33,7 @@ public class Main {
 			} else if (command.equals("article list")) {
 				System.out.println("게시글 리스트 기능을 구현합니다.");
 
-				if(articles.size() == 0) {
+				if (articles.size() == 0) {
 					System.out.println("게시글이 존재하지 않습니다.");
 					continue;
 				}
@@ -39,24 +41,62 @@ public class Main {
 				for (Article article : articles) {
 					System.out.println(article.articleId + " | " + article.title);
 				}
+			} else if (command.startsWith("article detail ")) {
+				System.out.println("상세 게시글 기능을 구현합니다.");
+
+				command = command.trim();
+				String[] commandBits = command.split(" ");
+
+				if (commandBits.length != 3) {
+					System.out.println("잘못된 명령어입니다..");
+					continue;
+				}
+
+				String checkStr = commandBits[2];
+				boolean checkInt = checkStr.matches("-?\\d+");
+
+				int foundId = 0;
+
+				if (checkInt) {
+					foundId = Integer.parseInt(checkStr);
+				} else {
+					System.out.println("잘못된 명령어입니다.");
+				}
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.articleId == foundId) {
+						foundArticle = article;
+					}
+				}
+				if (foundArticle == null) {
+					System.out.println("게시글이 존재하지 않습니다.");
+					continue;
+				}
+
+				System.out.println("제목 : " + foundArticle.title);
+				System.out.println("번호 : " + foundArticle.articleId);
+				System.out.println("작성날짜 : " + foundArticle.regDate);
+				System.out.println("내용 : " + foundArticle.body);
 			} else {
-				System.out.println("잘못된 명령어입니다");
+				System.out.println("잘못된 명령어입니다.");
 				continue;
 			}
 		}
 	}
 }
-
 class Article {
 	static int index;
 	int articleId;
 	String title;
 	String body;
+	String regDate;
 
 	Article(String title, String body) {
 		this.index++;
 		this.articleId = index;
 		this.title = title;
 		this.body = body;
+		this.regDate = Util.getNowDateStr();
 	}
 }
